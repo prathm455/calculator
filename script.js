c4ae36557f198f91890209c5329c857cac87f330
prathm455 <prathmeshm804@gmail.com>
2026-02-19 20:42:02 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let string="";
+let buttons=document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=='='){
+            string=eval(string);
+            document.getElementById('input').value=string;
+        }
+        else if(e.target.innerHTML=='AC'){
+            string="";
+            document.getElementById('input').value=string;
+        }
+        else{
+            string=string+e.target.innerHTML;
+            document.getElementById('input').value=string;
+        }
+
+
+    })
+})
